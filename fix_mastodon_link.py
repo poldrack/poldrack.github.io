d80aff52ebc652e5a332c9aae0e82251a056241a
Russell Poldrack <poldrack@gmail.com>
2022-11-24 09:00:42 -0800

    initial add

---
+++ fix_mastodon_link.py
@@ -0,0 +1,10 @@
+with open('docs/index.html') as f:
+    html = f.read()
+
+html = html.replace(
+    '<a class="nav-link" href="https://mastodon.online/@Russpoldrack">',
+    '<a class="nav-link" href="https://mastodon.online/@Russpoldrack" rel="me">'
+)
+
+with open('docs/index.html', 'w') as f:
+    f.write(html)
